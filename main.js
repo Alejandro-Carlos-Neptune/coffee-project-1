d2f0ef7eb0c5ecd24eb7f5f3d4eda169e8fba3f4
Alejandro Cabrera <cabrera.alejandr26@gmail.com>
2021-04-26 14:27:53 -0500

    Worked with divs

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 "use strict"
 
 function renderCoffee(coffee) {
-    var html = '<tr class="coffee">';
+    var html = '<tr class="coffee2">';
     html += '<td>' + coffee.id + '</td>';
     html += '<td>' + coffee.name + '</td>';
     html += '<td>' + coffee.roast + '</td>';
@@ -9,7 +9,7 @@ function renderCoffee(coffee) {
 
     return html;
 }
-
+//This the function that goes through the coffees names.
 function renderCoffees(coffees) {
     var html = '';
     for(var i = coffees.length - 1; i >= 0; i--) {
@@ -24,6 +24,20 @@ function updateCoffees(e) {
     var filteredCoffees = [];
     coffees.forEach(function(coffee) {
         if (coffee.roast === selectedRoast) {
+            filteredCoffees.push(coffee);
+        }
+    });
+    tbody.innerHTML = renderCoffees(filteredCoffees);
+
+}function updateCoffeesSearch(e) {
+    e.preventDefault(); // don't submit the form, we just want to update the data
+    var selectedRoast = roastSearch.value;
+    var filteredCoffees = [];
+    coffees.forEach(function(coffee) {
+        if (coffee.roast.toLowerCase() === selectedRoast.toLowerCase()) {
+            filteredCoffees.push(coffee);
+        }
+        if (coffee.name.toLowerCase() === selectedRoast.toLowerCase()) {
             filteredCoffees.push(coffee);
         }
     });
@@ -48,10 +62,38 @@ var coffees = [
     {id: 14, name: 'French', roast: 'dark'},
 ];
 
-var tbody = document.querySelector('#coffees');
-var submitButton = document.querySelector('#submit');
-var roastSelection = document.querySelector('#roast-selection');
+//This was used to reverse the object above.
+var coffees2 = coffees.reverse();
+console.log(coffees2);
 
-tbody.innerHTML = renderCoffees(coffees);
+var tbody = document.querySelector('#coffees2');
+var submitButton = document.querySelector('#submit');
+var submitButton2 = document.querySelector('#search-button');
+var roastSelection = document.querySelector('#roast-selection');
+var roastSearch = document.querySelector('#SearchCoffee');
+
+tbody.innerHTML = renderCoffees(coffees2);
 
 submitButton.addEventListener('click', updateCoffees);
+submitButton2.addEventListener('click', updateCoffeesSearch);
+
+
+// var coffeeInput = document.querySelector('#SearchCoffee');
+
+
+
+// const filterTest = coffees2.filter(coffee => coffee.name === 'city');
+// console.log(filterTest);
+// console.log(coffees.name);
+// console.log(coffees[5].name);
+
+
+// const searchBar = document.forms['Coffee-Search'].querySelector('input');
+// searchBar.addEventListener('keyup',function(e){
+//     const term = e.target.value.toLowerCase();
+//     const CoffeeNames = coffees2[0].name
+// })
+
+for(let i = 0; i < coffees2[7].name.length; i++){
+    console.log(coffees2[i].name);
+}
