d1aca3a8adaa00ed4bdf25f9ceb41943befc9770
Alejandro Cabrera <cabrera.alejandr26@gmail.com>
2021-04-26 17:38:18 -0500

    added a side image

--- main.js
+++ main.js
@@ -116,22 +116,7 @@ function myFunction() {
             li[i].style.display = "none";
         }
     }
-}function myFunction2() {
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById("myInput2");
-    filter = input.value.toLowerCase();
-    ul = document.getElementById("Roast");
-    li = ul.getElementsByTagName("li");
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toLowerCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-        }
-    }
 }
 
+
 myFunction();
-myFunction2();
